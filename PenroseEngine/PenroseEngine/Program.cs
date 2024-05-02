2ca340507f7e996c8ba906b39dea55caf0cabbb5
DiaDoesDastardly <127331399+DiaDoesDastardly@users.noreply.github.com>
2024-05-02 11:59:17 -0600

    Converted into C# project

---
+++ PenroseEngine/PenroseEngine/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PenroseEngine;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        gameObject cube = new gameObject("C:\\Users\\Charley Dodgion\\Penrose-Engine\\PenroseEngine\\PenroseEngine\\cube.obj");
+        double[,] rotationMatrix = rendererPipeline.rotationMatrixGenerator(0,0);
+        MyForm form = new MyForm(rotationMatrix,cube,100);
+        Application.Run(form);
+    }
+}
